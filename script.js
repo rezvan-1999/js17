543153c353a84482b214d5e5254b368a2995c030
rezvan khalili <rezvan.kh.1999@gmail.com>
2025-01-18 00:48:28 -0800

    the answer of q1

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 function todoList(todos) {
-    // Write your code here...
+  // Write your code here...
+  const div = document.querySelector("#content");
+  const h1 = document.createElement("h1");
+  h1.innerText = "Todo List";
+  const ul = document.createElement("ul");
+
+  for (const item of todos) {
+    const li = document.createElement("li");
+    li.innerText = item.todo;
+    ul.append(li);
   }
-  
-  const todos = [
-    {todo: "wash the dishes"}, 
-    {todo: "walk the dog"}, 
-    {todo: "learn javascript"}, 
-    {todo: "go shopping"}
-  ];
-  
-  todoList(todos);
+
+  div.append(h1, ul);
+}
+
+const todos = [
+  { todo: "wash the dishes" },
+  { todo: "walk the dog" },
+  { todo: "learn javascript" },
+  { todo: "go shopping" },
+];
+
+todoList(todos);
