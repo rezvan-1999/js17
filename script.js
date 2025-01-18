53abec56561413553e7d52f9392fd26e5fd20661
rezvan khalili <rezvan.kh.1999@gmail.com>
2025-01-18 01:03:14 -0800

    the answer of q2

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 function listOfColours(colours) {
-    // Write your code here...
+  // Write your code here...
+  const div = document.querySelector("#content");
+  const h3 = document.createElement("h3");
+  h3.innerText = "List of colors";
+  const ul = document.createElement("ul");
+  for (let i = 0; i < colours.length; i++) {
+    const li = document.createElement("li");
+    li.innerText = colours[i];
+    ul.append(li);
   }
-  
-  const colours = ["red", "blue", "green", "yellow", "pink", "brown"];
-  
-  listOfColours(colours);
-  
+
+  div.append(h3, ul);
+}
+
+const colours = ["red", "blue", "green", "yellow", "pink", "brown"];
+
+listOfColours(colours);
